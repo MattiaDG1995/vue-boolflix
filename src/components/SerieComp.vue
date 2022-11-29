f4a7337a26808a0e4ea15eea32df9dd47e586089
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-29 18:18:27 +0100

    fix card

--- src/components/SerieComp.vue
+++ src/components/SerieComp.vue
@@ -4,8 +4,8 @@
         <div class="card bg-dark text-white" style="width: 18rem;">
             <img :src="linkImg + val.poster_path" class="card-img-top" alt="...">
             <div class="card-body">
-                <h4 class="card-title">Titolo: {{val.title}}</h4>
-                <h5 class="card-title">Titolo Originale: {{val.original_title}}</h5>
+                <h4 class="card-title">Titolo: {{val.name}}</h4>
+                <h5 class="card-title">Titolo Originale: {{val.original_name}}</h5>
                 <h5 class="card-title">Lingua: {{val.original_language}}</h5>
                 <p class="card-text">Voto: {{val.vote_average}}</p>              
             </div>
